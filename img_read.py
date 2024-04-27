af7e0598ee227b4f6771f7a87771de55a4d39cd0
ashiknazar <warticcotaky@gmail.com>
2024-04-27 16:28:42 +0530

    added image pyramids

--- img_read.py
+++ img_read.py
@@ -1,13 +1,29 @@
 import cv2
-print(cv2.__version__)
 
-img=cv2.imread('data/kris.jpg',0)
-cv2.imshow('Image', img)
+# Read the image
+img = cv2.imread("data/kris.jpg",1)
 
-# Wait for any key to be pressed indefinitely
-cv2.waitKey(0)
-# Close all OpenCV windows
+# Construct Gaussian pyramid
+G = img.copy()
+gp = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gp.append(G)
+
+# Upsample the last level of Gaussian pyramid
+GE = cv2.pyrUp(gp[5])
+
+# Subtract the upsampled image from the next level of Gaussian pyramid
+L = cv2.subtract(gp[4], GE)
+
+# Display the original image
+cv2.imshow('Original Image', img)
+
+# Display the upsampled image
+cv2.imshow('Upsampled Image', GE)
+
+# Display the Laplacian image
+cv2.imshow('Laplacian Image', L)
+
+cv2.waitKey(0) & 0xFF
 cv2.destroyAllWindows()
-
-#save new copys
-cv2.imwrite('data/kris_copy.png',img)
